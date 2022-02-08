1cb949e35abc98076f6bb797d854a802ed7215a3
yerovyespitia <luyeesgar@gmail.com>
2022-02-08 14:14:33 -0500

    fetching with axios

--- pages/games/[id].jsx
+++ pages/games/[id].jsx
@@ -8,45 +8,15 @@ import addIcon from "../../public/images/add-icon.svg";
 import { useState } from "react";
 import axios from "axios";
 import GameState from "../../components/GameState";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export async function getStaticPaths() {
-  const res = await fetch(`${process.env.API_URL}api/games?page=1&limit=0`);
-  const data = await res.json();
-  const paths = data.map((id) => {
-    return {
-      params: { id: id.title.toString() },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const [res, res2] = await Promise.all([
-    fetch(`${process.env.API_URL}api/games/${id}`),
-    fetch(`${process.env.API_URL}api/comments/${id}?page=1&limit=25`),
-  ]);
-  const [data, data2] = await Promise.all([
-    res.json(),
-    res2.json()
-  ])
-
-  return {
-    props: {
-      game: data,
-      comments: data2,
-    },
-  };
-};
-
-const GameName = ({ game, comments }) => {
-  const router = useRouter()
-  const { id } = router.query
+const GameName = () => {
+  const router = useRouter();
+  const { id } = router.query;
   const user = useSelector((state) => state.user.value);
+  const [game, setGame] = useState([]);
+  const [comments, setComments] = useState([]);
   const [addReportActive, setAddReportActive] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -55,7 +25,23 @@ const GameName = ({ game, comments }) => {
   const [launcher, setLauncher] = useState("");
   const [macUsed, setMacUsed] = useState("");
 
-  const handleSubmit = (e) => {
+  useEffect(async () => {
+    const res = await axios.get(`${process.env.API_URL}api/games/${id}`);
+    setGame(res.data);
+  }, []);
+
+  useEffect(async () => {
+    const res = await axios.get(
+      `${process.env.API_URL}api/comments/${id}?page=1&limit=25`
+    );
+    if (!res.data[0]._id) {
+      setComments([]);
+    } else {
+      setComments(res.data);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newComment = {
       username: user.user.username,
@@ -67,10 +53,7 @@ const GameName = ({ game, comments }) => {
       macUsed,
     };
     try {
-      axios.post(
-        `${process.env.API_URL}api/comments/${game.title}`,
-        newComment
-      );
+      await axios.post(`${process.env.API_URL}api/comments/${id}`, newComment);
       setAddReportActive(!addReportActive);
       window.location.replace("/");
     } catch (error) {}
@@ -92,16 +75,18 @@ const GameName = ({ game, comments }) => {
         <title>{id} | State of Mac</title>
       </Head>
       <div className={styles.gamesImgContainer}>
-        <Image
-          src={game.wallpaper}
-          width={1454}
-          height={813}
-          className={styles.gamesImg}
-          alt="wallpaper"
-        />
+        {game.wallpaper && (
+          <Image
+            src={game.wallpaper}
+            width={1454}
+            height={813}
+            className={styles.gamesImg}
+            alt="wallpaper"
+          />
+        )}
       </div>
       <div className={styles.gamesContactContainer}>
-        <h1>{game.title}</h1>
+        <h1>{id}</h1>
         <GameState game={game.state} />
       </div>
       <div className={styles.gamesCommentsContainer}>
@@ -143,9 +128,8 @@ const GameName = ({ game, comments }) => {
             <div className={styles.addNewReportOptions}>
               <select onChange={(e) => setRunThrough(e.target.value)}>
                 <option
-                  value=""
                   disabled
-                  selected="true"
+                  selected={true}
                   defaultValue={"Game Run Through"}
                 >
                   Game Run Through
@@ -161,9 +145,8 @@ const GameName = ({ game, comments }) => {
               </select>
               <select onChange={(e) => setState(e.target.value)}>
                 <option
-                  value=""
                   disabled
-                  selected="true"
+                  selected={true}
                   defaultValue={"State of the Game"}
                 >
                   State of the Game
@@ -173,12 +156,7 @@ const GameName = ({ game, comments }) => {
                 <option value="Unplayable">Unplayable</option>
               </select>
               <select onChange={(e) => setLauncher(e.target.value)}>
-                <option
-                  value=""
-                  disabled
-                  selected="true"
-                  defaultValue={"Launcher"}
-                >
+                <option disabled selected={true} defaultValue={"Launcher"}>
                   Launcher
                 </option>
                 <option value="Steam Launcher">Steam Launcher</option>
@@ -192,7 +170,7 @@ const GameName = ({ game, comments }) => {
                 <option value="None">None</option>
               </select>
               <select onChange={(e) => setMacUsed(e.target.value)}>
-                <option value="" disabled selected="true" defaultValue={"Mac"}>
+                <option disabled selected={true} defaultValue={"Mac"}>
                   Mac
                 </option>
                 <option value="MacBook Pro M1 2020">MacBook Pro M1 2020</option>
@@ -223,8 +201,9 @@ const GameName = ({ game, comments }) => {
             </div>
           </form>
         )}
-        {comments[0].comment &&
-          comments.map((c, i) => <Comment comment={c} key={i} />)}
+        {comments.map((c, i) => (
+          <Comment comment={c} key={i} />
+        ))}
       </div>
     </div>
   );
