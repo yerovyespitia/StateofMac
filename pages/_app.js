e4457f411fe6cc2edb92f5310e2c7173caab635d
yerovyespitia <luyeesgar@gmail.com>
2022-02-09 09:05:39 -0500

    google analytics error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,17 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ID}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', '${process.env.GOOGLE_ID}');
+                `}
+      </Script>
     </Provider>
   );
 }
