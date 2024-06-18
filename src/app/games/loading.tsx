8bc398c2e78914bac0a0014830e9a829e91c546e
Yerovy Espitia <luyeesgar@gmail.com>
2024-06-18 00:36:49 -0500

    making loading page for games

---
+++ src/app/games/loading.tsx
@@ -0,0 +1,23 @@
+export default function Loading() {
+  return (
+    <section className='mx-3 md:mx-auto md:my-0 md:max-w-fit'>
+      {/* {Array.from({ length: 15 }).map(() => (
+        <article className='mb-4'>
+          <div className='animate-pulse'>
+            <section className='mt-3 flex flex-col items-center w-[849px] h-[257px] cursor-pointer rounded-lg bg-[#292929] md:flex-row'>
+              <section className='mx-0 mb-2 mt-6 px-3 md:m-5 md:h-auto md:w-auto md:px-0'>
+                <div className='rounded-lg w-[374px] h-[221px] animate-pulse bg-[#363636]' />
+              </section>
+
+              <section className='flex flex-col items-center justify-center text-center md:mr-4 md:block md:text-left'>
+                <div className='my-1 animate-pulse w-[232px] h-[32px] bg-[#363636] rounded-lg mb-3' />
+                <div className='mx-0 my-1 animate-pulse w-[232px] h-[32px] bg-[#363636] rounded-lg mb-3' />
+                <div className='mx-0 my-1 animate-pulse w-[232px] h-[32px] bg-[#363636] rounded-lg mb-3' />
+              </section>
+            </section>
+          </div>
+        </article>
+      ))} */}
+    </section>
+  )
+}
